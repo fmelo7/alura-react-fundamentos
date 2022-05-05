c8accfa419110f90a147c9d9fe43aea15acc69fd
Francisco Melo <franciscomelo@oi.net.br>
2022-05-05 14:22:15 -0300

    Controlando o formulário

--- src/components/formulario/index.tsx
+++ src/components/formulario/index.tsx
@@ -3,16 +3,44 @@ import Botao from '../botao';
 import style from './formulario.module.scss';
 
 class Formulario extends React.Component {
+    state = {
+        tarefa: '',
+        tempo: '00:00',
+    };
+
+    adicionarTarefa(evento: React.FormEvent<HTMLFormElement>) {
+        evento.preventDefault();
+        console.log('state', this.state);
+    }
+
     render() {
         return (
-            <form action='' className={style.novaTarefa}>
+            <form onSubmit={this.adicionarTarefa.bind(this)} className={style.novaTarefa}>
                 <div className={style.inputContainer}>
                     <label htmlFor='tarefa'>Adicione um novo estudo: </label>
-                    <input type='text' id='tarefa' name='terefa' placeholder='O que voce quer estudar?' required></input>
+                    <input
+                        type='text'
+                        id='tarefa'
+                        name='terefa'
+                        value={this.state.tarefa}
+                        onChange={(evento) => this.setState({ ...this.state, tarefa: evento.target.value })}
+                        placeholder='O que voce quer estudar?'
+                        required
+                    ></input>
                 </div>
                 <div className={style.inputContainer}>
                     <label htmlFor='tempo'>Tempo</label>
-                    <input type='time' id='tempo' name='tempo' step='1' min='00:00:00' max='01:30:00' required></input>
+                    <input
+                        type='time'
+                        id='tempo'
+                        name='tempo'
+                        value={this.state.tempo}
+                        onChange={(evento) => this.setState({ ...this.state, tempo: evento.target.value })}
+                        step='1'
+                        min='00:00:00'
+                        max='01:30:00'
+                        required
+                    ></input>
                 </div>
                 <div className={style.inputContainer}>
                     <Botao>Adicionar</Botao>
