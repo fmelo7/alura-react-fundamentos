70ab0286cbe35d6e33281c29ce68460ec27383e5
Francisco Melo <franciscomelo@oi.net.br>
2022-06-06 18:09:25 -0300

    Regressiva

--- src/components/cronometro/index.tsx
+++ src/components/cronometro/index.tsx
@@ -11,18 +11,29 @@ interface Props {
 
 export default function Cronometro({ selecionado }: Props) {
     const [tempo, setTempo] = useState<number>();
+
     useEffect(() => {
         if (selecionado?.tempo) {
             setTempo(tempoParaSegundos(selecionado.tempo));
         }
     }, [selecionado]);
+
+    function regressiva(contador: number = 0) {
+        setTimeout(() => {
+            if (contador > 0) {
+                setTempo(contador - 1);
+                regressiva(contador - 1);
+            }
+        }, 1000);
+    }
+
     return (
         <div className={style.cronometro}>
             <p className={style.titulo}>Escolha um card e inicie o cronometro</p>
             <div className={style.relogioWrapper}>
                 <Relogio tempo={tempo} />
             </div>
-            <Botao>Começar!</Botao>
+            <Botao onClick={() => regressiva(tempo)}>Começar!</Botao>
         </div>
     );
 }
